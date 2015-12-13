3a6a697023f7558d477d77377b3977d74f9a5dc2
Caro <carolin.stellmacher@haw-hamburg.de>
2015-12-13 17:19:50 +0100

    GUI elements and convert int to String

--- whadug/whadug/Main.cpp
+++ whadug/whadug/Main.cpp
@@ -7,7 +7,7 @@ using namespace cv;
 double smallArea;
 double bigArea;
 
-void compareAreas(double areaOne, double areaTwo){
+void compareAreas(float areaOne, float areaTwo){
 
 		if (areaOne < areaTwo){
 			smallArea = areaOne;
@@ -24,29 +24,54 @@ void compareAreas(double areaOne, double areaTwo){
 		}
 }
 
-void showTime(String text){
+String toString(int number){
 
+	String s;
+	stringstream out;
+	out << number;
+	s = out.str();
+	return s;
+
+}
+
+void showTime(int minutes, int hours){
+	//just for test
 	Mat img = imread("D:/_Daten_Carolin/_HAW/7_IMP/whadug/whadug/Blume.jpg", CV_LOAD_IMAGE_COLOR);
 
+	String label = toString(hours) + ":" + toString(minutes);
 
-	//addText(const Mat& img, text, Point org, CvFont font);
-	putText(img, text, Point(40, 80), FONT_HERSHEY_PLAIN, 2, Scalar(36, 22, 21), 2, 8, false);
-	namedWindow("Uhrzeit");
-	imshow("Uhrzeit", img);
+	// rectangle for GUI
+	Point RectUpLeft = Point(50, 0);
+	Point RectDownRight = Point(195, 220);
+	rectangle(img, RectUpLeft, RectDownRight, Scalar(40, 40, 40), CV_FILLED, 8, 0);
 
+	//text properties
+	int fontface = FONT_HERSHEY_SIMPLEX; //FONT_HERSHEY_PLAIN
+    double scale = 1.1;
+    int thickness = 1;
+    int baseline = 0;
+
+	// center label horizontally
+	Size text = getTextSize(label, fontface, scale, thickness, &baseline);
+	int textX = (((RectDownRight.x - RectUpLeft.x)/2) + RectUpLeft.x) - (text.width /2);
+	// vertical position
+	int textY = (RectDownRight.y - (RectDownRight.y / 5));
+	//display time
+	putText(img, label, Point(textX, textY), fontface, scale, Scalar(215, 215, 215), thickness, 8);
 	
+	namedWindow("Uhrzeit");
+	imshow("Uhrzeit", img);	
 }
 
 void main(){
 
-	double areaOne = 21; // = contourArea(*shape);
-	double areaTwo = 22; 
+	float areaOne = 21.5;
+	float areaTwo = 22.0; 
 	compareAreas(areaOne, areaTwo);
 
-	//ostringstream time;
-	String time = "Uhrzeit";
-	//time = "Uhrzeit";
-	showTime(time);
+	int minutes = 58;
+	int hours = 23;
+	showTime(minutes, hours);
 
 	waitKey(0);
 }
