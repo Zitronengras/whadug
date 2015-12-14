f57a8b2c4824058f28f8334c154a0155195fa7fd
Trokan <mail@simonpontius.de>
2015-12-14 16:41:38 +0100

    Merge code step one
    Merge code step one

--- whadug/whadug/Main.cpp
+++ whadug/whadug/Main.cpp
@@ -14,12 +14,19 @@ float calculateRectangleCenterX(float point1X, float point1Y, float point2X, flo
 float calculateRectangleCenterY(float point1X, float point1Y, float point2X, float point2Y);
 float calculateTriangleCenterX(float point1X, float point1Y, float point2X, float point2Y, float point3X, float point3Y);
 float calculateTriangleCenterY(float point1X, float point1Y, float point2X, float point2Y, float point3X, float point3Y);
+void runCameraAndCircle();
+void runCompareArea();
+void compareAreas(float areaOne, float areaTwo);
+String toString(int number);
+void showTime(int minutes, int hours);
 
-
+double smallArea, bigArea;
+	
 int main(){
 
 	char character;
 	float rectangleDistanceBetweenTwoPoints, rectangleCenterX, rectangleCenterY, triangleCenterX, triangleCenterY, x1, y1, x2, y2, x3, y3;
+
 	
 	//***************Calculation points***************
 	//Rectangle: P1(x1, y1) & P2(X2, y2)
@@ -40,6 +47,8 @@ int main(){
 	cout << "d: Eigene Punkte angeben fuer Rechteck" << endl;
 	cout << "e: Eigene Punkte angeben fuer Dreieck" << endl;
 	cout << "f: Programm starten" << endl;
+	cout << "g: Kreis erkennen und mit \"s\" einen Snapshot machen" << endl;
+	cout << "h: Uhrzeit und Flaecheninhalt anzeigen" << endl;
 
 	for(;;){
 		cout << "Eingabe des Buchstaben: ";
@@ -92,6 +101,14 @@ case 'a':
 
 			case 'f': 
 				runProgramm();
+			break;
+
+			case 'g': 
+				runCameraAndCircle();
+			break;
+
+			case 'h': 
+				runCompareArea();
 			break;
 
 			default: 
@@ -236,7 +253,7 @@ RNG rng(12345);
 
 
 }
-
+//****************************CODE SIMON********************************
 //***************FUNCTIONS***************
 float calculateRectangleDistanceBetweenTwoPoints(float point1X, float point1Y, float point2X, float point2Y){
 	//Point 1: (x1,y1) and Point 2: (x2, y2)
@@ -277,3 +294,174 @@ float calculateTriangleCenterY(float point1X, float point1Y, float point2X, floa
 	mY = (point1Y + point2Y + point3Y) / 3;
 	return mY;
 }
+//****************************CODE SIMON ENDE********************************
+
+//****************************CODE SONJA********************************
+void runCameraAndCircle(){
+	Mat img,
+	imgGray,
+	imgBinary,
+	imgCanny,
+	imgContours,
+	imgCircle;
+
+int radius;
+char filename[20];
+
+// Kamera
+	VideoCapture cap(0);		// default camera 
+	if(!cap.isOpened()){		// erfolgreich?
+		cout << "Kamera konnte nicht aktiviert werden." << endl;
+	}
+	for(;;){
+		cap >> img;
+		if(img.empty())
+			break;
+		
+		imshow("Camera", img);
+
+		char key = (char)waitKey(5);
+		switch (key){
+		case 83:
+		case 115:
+			sprintf(filename, "Clock.jpg");
+			imwrite(filename, img);
+			cout << "Saved " << filename << endl;
+			break;
+
+		default:
+			break;
+		}
+		if(waitKey(30) >= 0) break;
+	}
+
+	//img = imread("IMG_0351.jpg");
+	
+	cvtColor(img, imgGray, CV_BGR2GRAY);	// Graubild
+	blur(imgGray, imgGray, Size(3,3));		// mit 3x3 Filtermaske weichzeichnen
+	threshold(imgGray, imgBinary, 100, 255, THRESH_BINARY);	// Binärbild
+	Canny(imgBinary, imgCanny, 0, 200, 3);	// Kantenbild
+
+	// Konturen finden
+	vector<vector<Point>> contours;			// Kontur wird als Vektor aus Punkten abgespeichert
+	findContours(imgCanny, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
+
+	// Konturen blau zeichnen
+	imgContours = img.clone();
+	for(int i = 0; i < contours.size(); i++){
+		drawContours(imgContours, contours, i, Scalar(200, 0, 0), 2, 8);		
+	}
+	
+	cvtColor(imgGray, imgCircle, CV_GRAY2BGR);
+
+	// Kreise finden
+	vector<Vec3f> circles;		// xc, yc und r
+	HoughCircles(imgGray, circles, CV_HOUGH_GRADIENT, 1, imgGray.rows/8, 150, 20, 100, 0);
+
+	// gefundenen Kreis einzeichnen
+	for(int i = 0; i < circles.size(); i++){
+		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+		radius = cvRound(circles[i][2]);
+		circle(imgCircle, center, 4, Scalar(255, 0, 0), -1, 8, 0);
+		circle(imgCircle, center, radius, Scalar(255, 0, 255), 3, 8, 0);
+
+		cout << "Radius" << radius << endl;
+		cout << "Center" << center << endl;
+	}
+	
+	const char* image = "Clock";
+	const char* gray = "Gray";
+	const char* binary = "Binary";
+	const char* canny = "Canny";
+	const char* contour = "Contours";
+	const char* circleIMG = "Circles";
+	
+	namedWindow(image, WINDOW_AUTOSIZE);
+	namedWindow(gray, WINDOW_AUTOSIZE);
+	namedWindow(binary, WINDOW_AUTOSIZE);
+	namedWindow(canny, WINDOW_AUTOSIZE);
+	namedWindow(contour, WINDOW_AUTOSIZE);
+	namedWindow(circleIMG, WINDOW_AUTOSIZE);
+	
+	imshow(image, img);
+	imshow(gray, imgGray);
+	imshow(binary, imgBinary);
+	imshow(canny, imgCanny);
+	imshow(contour, imgContours);
+	imshow(circleIMG, imgCircle);
+
+	waitKey (0);
+}
+//****************************CODE SONJA ENDE********************************
+
+//****************************CODE CARO********************************
+void runCompareArea(){
+
+	float areaOne = 21.5;
+	float areaTwo = 22.0; 
+	compareAreas(areaOne, areaTwo);
+
+	int minutes = 58;
+	int hours = 23;
+	showTime(minutes, hours);
+
+	waitKey(0);
+}
+
+
+void compareAreas(float areaOne, float areaTwo){
+
+		if (areaOne < areaTwo){
+			smallArea = areaOne;
+			bigArea = areaTwo;
+			cout << "Grosser Zeiger: " << bigArea << " \nKleiner Zeiger: " << smallArea << endl;
+		}
+		else if(areaOne > areaTwo){
+			smallArea = areaTwo;
+			bigArea = areaOne;
+			cout << "Grosser Zeiger: " << bigArea << " \nKleiner Zeiger: " << smallArea << endl;
+		}
+		else{
+			cout << "Fehler bei Vergleich der Flächeninhalte" << endl;
+		}
+}
+
+String toString(int number){
+
+	String s;
+	stringstream out;
+	out << number;
+	s = out.str();
+	return s;
+
+}
+
+void showTime(int minutes, int hours){
+	//just for test
+	Mat img = imread("C:\uhr.jpg", CV_LOAD_IMAGE_COLOR);
+
+	String label = toString(hours) + ":" + toString(minutes);
+
+	// rectangle for GUI
+	Point RectUpLeft = Point(50, 0);
+	Point RectDownRight = Point(195, 220);
+	rectangle(img, RectUpLeft, RectDownRight, Scalar(40, 40, 40), CV_FILLED, 8, 0);
+
+	//text properties
+	int fontface = FONT_HERSHEY_SIMPLEX; //FONT_HERSHEY_PLAIN
+    double scale = 1.1;
+    int thickness = 1;
+    int baseline = 0;
+
+	// center label horizontally
+	Size text = getTextSize(label, fontface, scale, thickness, &baseline);
+	int textX = (((RectDownRight.x - RectUpLeft.x)/2) + RectUpLeft.x) - (text.width /2);
+	// vertical position
+	int textY = (RectDownRight.y - (RectDownRight.y / 5));
+	//display time
+	putText(img, label, Point(textX, textY), fontface, scale, Scalar(215, 215, 215), thickness, 8);
+	
+	namedWindow("Uhrzeit");
+	imshow("Uhrzeit", img);	
+}
+//****************************CODE CARO ENDE********************************
